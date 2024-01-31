228c967c8a0829cfb793a8a4696f8e4c34ff8414
Angel Beltran <beltranangel7293@gmail.com>
2024-01-31 14:01:16 -0800

    fixing little thing

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='font-serif text-5xl font-bold'>VERA&apos;S GENERAL BUILDING</h2>
-        <p className='py-5 text-2xl'>'We provide amazing, quality and creative Construction Designs!</p>
+        <p className='py-5 text-2xl'>We provide amazing, quality and creative Construction Designs!</p>
       </div>
     </div>
   );
